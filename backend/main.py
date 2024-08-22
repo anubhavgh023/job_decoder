1af54f0aefdd70c4f7c04bc54687249751b6659e
Anubhav <anubhavsamanta023@gmail.com>
2024-08-22 22:07:56 +0530

    refactor: /search route returning list in proper json format

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from fastapi import FastAPI, Query, HTTPException
 import sqlite3
+from collections import defaultdict
 
 app = FastAPI()
 
@@ -20,24 +21,67 @@ async def search(keyword: Annotated[str, Query()] = ...):
 
     # query keyword
     try:
-        # Connect to db
+        # connect to db
         conn = db_connect()
         cur = conn.cursor()
 
-        # Perform search using FTS5 virtual table
+        # perform search using FTS5 virtual table
         search_query = keyword.replace(" ", " OR ")
         query = """
-            SELECT company_id,company_name,job_id
-            FROM job_skill_search
-            WHERE job_skill_search MATCH ?
+            SELECT 
+                c.company_id,
+                c.company_name,
+                c.short_description,
+                c.website,
+                j.job_id,
+                j.title AS job_title,
+                j.job_url
+            FROM job_skill_search js
+            JOIN company c ON js.company_id = c.company_id
+            JOIN jobs j ON js.job_id = j.job_id
+            WHERE js.job_skill_search MATCH ?
         """
         results = cur.execute(query, (search_query,)).fetchall()
+
     except sqlite3.Error as e:
         conn.close()
         raise HTTPException(status_code=500, detail=f"Database search query error: {e}")
     finally:
         conn.close()
 
-    companies = [row["company_name"] for row in results]
+    # aggregate job details by company_id
+    company_data = defaultdict(
+        lambda: {
+            "company_id": None,
+            "company_name": "",
+            "short_description": "",
+            "website": "",
+            "jobs": [],
+        }
+    )
 
-    return {"companies": companies}
+    for row in results:
+        company_id = row["company_id"]
+
+        # get or create the company entry
+        company_entry = company_data[company_id]
+
+        if company_entry["company_id"] is None:
+            company_entry["company_id"] = row["company_id"]
+            company_entry["company_name"] = row["company_name"]
+            company_entry["short_description"] = row["short_description"]
+            company_entry["website"] = row["website"]
+
+        # append the job details to the company's job list
+        company_entry["jobs"].append(
+            {
+                "job_id": row["job_id"],
+                "job_title": row["job_title"],
+                "job_url": row["job_url"],
+            }
+        )
+
+    # converting response data to list
+    response_data = {"data": list(company_data.values())}
+
+    return response_data
