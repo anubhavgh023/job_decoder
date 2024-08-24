a3b1a1d995d7cd780670027551a2c187f887d967
Anubhav <anubhavsamanta023@gmail.com>
2024-08-24 17:38:17 +0530

    update: added cors middleware & included localhost:3000 origin

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,24 @@ from typing import Annotated
 from fastapi import FastAPI, Query, HTTPException
 import sqlite3
 from collections import defaultdict
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def db_connect():
     try:
