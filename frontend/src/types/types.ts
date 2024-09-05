c58398a3b9579540b3756b09a68d8b099353899b
Anubhav <anubhavsamanta023@gmail.com>
2024-09-05 08:16:05 +0530

    fix: combined all types in a single file

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,36 @@
+export interface Job {
+    job_id: number;
+    job_title: string;
+    job_url: string;
+}
+
+export interface Company {
+    company_id: number;
+    company_name: string;
+    short_description: string;
+    website: string;
+    jobs: Job[];
+}
+
+export type JobDensityData = {
+  country: string | null;
+  job_count: number;
+};
+
+export type Skill = {
+    name: string;
+    count: number;
+};
+
+export type DashboardData = {
+    geo_data: JobDensityData[];
+    top_skills: {
+        languages: Skill[];
+        tools: Skill[];
+    };
+}
+
+// Define type for component props
+export type GeoMapProps = {
+  data: JobDensityData[];
+};
